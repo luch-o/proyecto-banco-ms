3f55442915e44a9f6523c07af7ad64967212d8d3
luch-o <luiscarrodba97@gmail.com>
2021-08-12 20:48:29 -0500

    created API gateway project

---
+++ APIGateway/src/server.js
@@ -0,0 +1,4 @@
+module.exports = {
+    users_api_url: process.env.USERS_API_URL || 'https://cuentams.herokuapp.com/',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://c4-account-ms.herokuapp.com/',
+};
