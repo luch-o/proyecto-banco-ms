8a3cb855add86c4e7d6cbdbcf46e825738e0d918
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-08-31 08:15:30 -0500

    webapp project created with vue

---
+++ WebApp/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+  body{
+    margin: 0 0 0 0;
+  }
+
+  .header{
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100px;
+    margin: 0%;
+    padding: 0;
+
+    background-color: #283747 ;
+    color:#E5E7E9  ;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+
+  .header nav {
+    height: 100%;
+    width: 40%;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 20px;
+  }
+
+  .header nav button{
+    color: #E5E7E9;
+    background: #283747;
+    border: 1px solid #E5E7E9;
+
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
+
+  .header nav button:hover{
+    color: #283747;
+    background: #E5E7E9;
+    border: 1px solid #E5E7E9;
+  }
+
+  .main-component{
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+
+    background: #FDFEFE ;
+  }
+ 
+  .footer{
+    position: fixed;
+    left: 0px;
+    bottom: 0px;
+    width: 100%;
+    height: 75px;
+
+    background-color: #283747;
+    color: #E5E7E9;
+  }
+
+  .footer h2{
+    margin: 0px;
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ WebApp/src/components/User.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+    #User{
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        justify-content: center;    
+        align-items: center;
+    }
+
+    #User h2{
+        font-size: 50px;
+        color: #283747;
+    }
+
+    #User span{
+        color: crimson;
+        font-weight: bold;
+    }
+</style>
